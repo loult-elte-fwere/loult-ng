b422f0bc6f2463157df67b93e01c743aae3f0544
= <haspagan@users.noreply.github.com>
2019-11-19 10:17:37 -0600

    routing_table outside factory - to no avail

--- tools/admin/admin.py
+++ tools/admin/admin.py
@@ -4,19 +4,24 @@ from ..handlers import cookie_check
 
 admin = Quart(__name__)
 
-@cookie_check(MOD_COOKIES)
+# @cookie_check(MOD_COOKIES)
 @admin.route('/')
 async def admin_main():
     state = admin.config['state']
     req = request.headers.get("Cookie")
     return await render_template('main.html', state=state)
 
-@cookie_check(MOD_COOKIES)
+# @cookie_check(MOD_COOKIES)
 @admin.route('/manage', methods=['POST'])
 async def admin_user_manage():
     state = admin.config['state']
-    form = await request.form #multidict
-    # ... handle ban type and duration here, but how?
-    return await render_template('main.html', state=state, message="Action effectuée")
+    table = admin.config['table']
+    form = await request.form
+    print(form)
+    action = form.get('action')
+    if action is not None or action is not "":
+        if action == 'ban':
+            await table.route_json({"action" : "apply", "mod" : "trash", "userid" : form.get('userid')})
+            return await render_template('main.html', state=state, message="Action effectuée")
     
     
